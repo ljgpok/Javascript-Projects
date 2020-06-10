0aa414e461018d605e25fedd535011bc383c2961
Lal Gurung <ljgpok@yahoo.com>
2020-06-10 23:00:33 +0300

    project home page setup

---
+++ script.js
@@ -0,0 +1,42 @@
+const projects = [
+	{
+		name: 'form-validator',
+		github:
+			'https://github.com/t1gula00/Javascript-Projects/tree/master/form-validator'
+	},
+	{
+		name: 'movie-booking',
+		github:
+			'https://github.com/t1gula00/Javascript-Projects/tree/master/movie-booking'
+	}
+];
+
+const list = document.getElementById('list');
+
+projects.forEach(({ name, github }, i) => {
+	const listItem = document.createElement('li');
+
+	listItem.innerHTML = `
+		<a href="/${name}/index.html">
+			<img src="/${name}/image.png" alt="${name}" />
+			<p>${i + 1}. ${formatProjectName(name)}</p>
+		</a>
+		<div class="links-container">
+			<a href="/${name}/index.html" class="blue">
+				<i class="fas fa-eye"></i>
+			</a>
+			<a href="${github}" class="github">
+				<i class="fab fa-github"></i>
+			</a>
+		</div>
+	`;
+
+	list.appendChild(listItem);
+});
+
+function formatProjectName(name) {
+	return name
+		.split('-')
+		.map(word => word[0].toUpperCase() + word.slice(1))
+		.join(' ');
+}
